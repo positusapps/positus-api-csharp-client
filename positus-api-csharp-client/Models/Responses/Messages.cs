335b2d27b74b8e54bd7fb77697cb412c0359e6e6
Richard Dias <richard.dias@multilog.com.br>
2022-06-20 14:44:16 -0300

    modified:   positus-api-csharp-client/Models/Requests/Template.cs Included possibility to send documents using templates
    modified:   positus-api-csharp-client/Models/Responses/Messages.cs
    Model increased to show detailed returns when receive some errors

--- positus-api-csharp-client/Models/Responses/Messages.cs
+++ positus-api-csharp-client/Models/Responses/Messages.cs
@@ -13,5 +13,26 @@ namespace positus_api_csharp_client.Models.Responses
     {
         public IList<MessageDetail> messages { get; set; }
         public string message { get; set; }
+        public Trace trace { get; set; }
+        public IList<Error> Errors { get; set; }
+    }
+
+    public class ServerType
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Trace
+    {
+        public Guid id { get; set; }
+        public ServerType server_type { get; set; }
+    }
+
+    public class Error
+    {
+        public int code { get; set; }
+        public string title { get; set; }
+        public string details { get; set; }
     }
 }
